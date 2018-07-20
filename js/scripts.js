46e5b5557c2b9f443e27e27bee0fdb2be149e608
billowbashir <billowbashir@gmail.com>
2018-07-20 18:08:38 +0300

    use fadeIn and hide

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
+//business
 var rollDice=function(){
 return  Math.floor((Math.random() * 6) + 1);
 }
+//user interface
 $(document).ready(function(){
-  $(".rolldice").submit(function(event){
+$("form#opponent").submit(function(){
+  event.preventDefault();
+  if (document.getElementById("player2").checked) {
+    $(".game").fadeIn();
+    $(".opponent").hide();
+  }else {
+    alert("computer is not available")
+  }
+})
+  $(".rolldice").submit(function(){
     event.preventDefault();
     rollDice();
 $(".total").text(rollDice());
   })
-})
+});
