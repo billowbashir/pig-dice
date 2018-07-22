ee405463637ed549ec020e9488fbe4fbe3fe48f0
billowbashir <billowbashir@gmail.com>
2018-07-22 22:11:25 +0300

    cant display names

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 //business
+function Player(name) {
+  this.roll = 0;
+  this.currentScore = 0;
+  this.totalScore = 0;
+  this.playerName = name;
+}
+var player1
+var player2
 var rollDice=function(){
 return  Math.floor((Math.random() * 6) + 1);
 }
@@ -6,16 +14,30 @@ return  Math.floor((Math.random() * 6) + 1);
 $(document).ready(function(){
 $("form#opponent").submit(function(){
   event.preventDefault();
-  if (document.getElementById("player2").checked) {
+  if (document.getElementById("player").checked) {
     $(".game").fadeIn();
     $(".opponent").hide();
   }else {
     alert("computer is not available")
   }
 })
-  $(".rolldice").submit(function(){
+  $(".player1rolldice").submit(function(){
     event.preventDefault();
     rollDice();
-$(".total").text(rollDice());
+$(".player1total").text(rollDice());
   })
+  $(".player2rolldice").submit(function(){
+    event.preventDefault();
+    rollDice();
+$(".player2total").text(rollDice());
+  })
+  $("form.fillName").submit(function(event){
+event.preventDefault();
+  var inputtedPlayer1Name=$("#inputtedPlayer1Name").val();
+  var inputtedPlayer2Name=$("#inputtedPlayer2Name").val();
+  player1=new Player(inputtedPlayer1Name)
+  player2=new Player(inputtedPlayer2Name)
+  $("#player1name").text(player1.name);
+  $("#player2name").text(player2.name);
+    })
 });
